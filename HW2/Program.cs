134751b9ce94eb9c59043dad7a2eecbb728e7179
mich026 <119221428+mich026@users.noreply.github.com>
2022-12-12 11:35:12 +0300

    Update Program.cs

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,15 +1,1 @@
-﻿int CutNumber(int num)
-{
-    int sot = num / 100;
-    int ed = num % 10;
 
-    int result = sot * 10 + ed;
-    return result;
-}
-
-int randNumb = new Random().Next(100, 1000);
-int newNumb = CutNumber(randNumb);
-
-Console.WriteLine($"New version of a number {randNumb} is {newNumb}");
-
-тест
