2e9c67daa6f3f6919c18ac8fad959305a756f81b
MariannaChernova <mich026@yandex.ru>
2022-12-14 22:19:14 +0300

    Задача 2

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1. 
-
+/*
 Console.WriteLine("введите число A");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число B");
@@ -11,3 +11,18 @@ for (int i = 1; i < b; i++)
     step = step * a;
 }
 Console.WriteLine("A в степени B равно: " + step);
+*/
+
+// Задача 2.
+
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
