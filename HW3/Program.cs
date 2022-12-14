544bd090b2e8e4c40de727a4da48cc53be9cba2c
MariannaChernova <mich026@yandex.ru>
2022-12-14 20:13:33 +0300

    Задача 2

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1
-
+/*
 void Palindrom(int number)
 {
     int num1 = number / 10000 % 10;
@@ -23,3 +23,30 @@ void Palindrom(int number)
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Palindrom(number);
+*/
+
+// Задача 2. 
+
+double Dist(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+double x = x2 - x1;
+double y = y2 - y1;
+double z = z2 - z1;
+double gip = Math.Pow(x,2) + Math.Pow(y,2) + Math.Pow(z,2);
+double result = Math.Sqrt(gip);
+return result;
+}
+Console.Write("Введите координату Х для А: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y для А: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z для А: ");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Х для B: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y для B: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z для B: ");
+double zB = Convert.ToDouble(Console.ReadLine());
+double res = Math.Round(Dist(xA, yA, xB, yB, zA, zB),2);
+Console.WriteLine($"Расстояние между А и В {res}");
