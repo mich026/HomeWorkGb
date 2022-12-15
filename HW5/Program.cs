e7a8c5af69e75c427151110ff21ead528da37dd5
MariannaChernova <mich026@yandex.ru>
2022-12-15 20:51:05 +0300

    Задача 2

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 1.
-
+/*
 Console.WriteLine("Введите длину массива:  ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
@@ -33,5 +33,38 @@ void PrintArray(int[] numbers)
         Console.Write(numbers[i] + " ");
     }
     Console.Write("] ");
+    Console.WriteLine();
+}*/
+
+// Задача 2.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int x = 0; x < numbers.Length; x+=2)
+    sum = sum + numbers[x];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
     Console.WriteLine();
 }
