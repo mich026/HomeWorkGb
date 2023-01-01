f89d8d42dd373b656b55e0efdd8f1810d463e7bf
MariannaChernova <mich026@yandex.ru>
2023-01-01 14:15:51 +0300

    Задача 2

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Задача 1.
+
 /*
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
 */
-
+/*
 void PrintNumbers(int num)
 {   
      if (num > 0)
@@ -17,3 +18,32 @@ void PrintNumbers(int num)
  Console.Write("Input a number: ");
  int n = Convert.ToInt32(Console.ReadLine());
  PrintNumbers(n);
+ */
+
+ // Задача 2.
+ /*
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30  
+*/
+
+     int SumNumbers(int m, int n)
+     {
+          if(m < n)
+          { 
+             return m + SumNumbers(m + 1,n);
+          }
+          if(m > n)
+          {  
+             return m + SumNumbers(m - 1,n);
+          }
+
+        return n;       
+     }
+    
+     Console.Write("Enter number M: ");
+     int m = Convert.ToInt32(Console.ReadLine());
+     Console.Write("Enter number N: ");
+     int n = Convert.ToInt32(Console.ReadLine());
+     int res = SumNumbers(m,n);
+     Console.WriteLine("Sum: " + (res));
+
