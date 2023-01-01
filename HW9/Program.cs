c05f036e5b46e062c60652287c8148587092bada
MariannaChernova <mich026@yandex.ru>
2023-01-01 14:07:01 +0300

    Задача 1

---
+++ HW9/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1.
+/*
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+*/
+
+void PrintNumbers(int num)
+{   
+     if (num > 0)
+     {  
+        Console.Write(num + " ");
+        PrintNumbers(num - 1);
+     }
+     
+ }
+
+ Console.Write("Input a number: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ PrintNumbers(n);
